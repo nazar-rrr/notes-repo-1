760fd908d9f954e483c41257e2037a7b7803cd50
nazar-rrrrrr <161465659+nazar-rrrrrr@users.noreply.github.com>
2024-04-19 11:06:04 +0000

    Updated CSS

--- js/main-navigation.js
+++ js/main-navigation.js
@@ -1,1 +1,22 @@
-const theButton = document.querySelector('')
+const theButton = document.querySelector('.main-navigation__button')
+const theMainNav = document.querySelector('.main-navigation')
+const theMainNavContainer = document.querySelector('.main-navigation__container')
+
+const manipulateMainNav = () => {
+    if (theMainNav.classList.contains('hidden')) {
+        theButton.style.bottom = '190px';
+        theMainNav.classList.remove('hidden');
+        theMainNavContainer.classList.remove('main-navigation__container--disappear')
+        theMainNavContainer.classList.add('main-navigation__container--appear')
+    }
+    else {
+        theMainNavContainer.classList.remove('main-navigation__container--appear')
+        theMainNavContainer.classList.add('main-navigation__container--disappear')
+        setTimeout(() => {
+            theMainNav.classList.add('hidden');
+        }, 200)
+        theButton.style.bottom = '10px';
+    }
+}
+
+theButton.addEventListener('click', manipulateMainNav);
