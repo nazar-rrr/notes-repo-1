fa23118d0b1db5d1735c9c6ac7a7a9d5003f0caf
nazar-rrrrrr <161465659+nazar-rrrrrr@users.noreply.github.com>
2024-04-20 12:45:28 +0000

    Updade JS Section

--- js/rendering-todo-page.js
+++ js/rendering-todo-page.js
@@ -2,6 +2,7 @@ const theHomePageButton = document.querySelector('.home-page__heading-button');
 const theHomePage = document.querySelector('.home-page');
 const theMainNavigationButton = document.querySelector('.main-navigation__todo-page');
 const theToDoPage = document.querySelector('.todo-page');
+const theToDoPageTasksFieldContainer = document.querySelector('.tasks__field-container');
 
 const removeHomePage = () => {
     theHomePage.classList.add('home-page__disappear');
@@ -12,7 +13,11 @@ const removeHomePage = () => {
 
 const renderToDoPage = () => {
     theToDoPage.classList.remove('hidden');
+    theToDoPageTasksFieldContainer.classList.remove('tasks__field-container-appeaar')
     theToDoPage.classList.add('todo-page__appear');
+    setTimeout(() => {
+        theToDoPageTasksFieldContainer.classList.add('tasks__field-container--appear')
+    }, 200)
 }
 
 const manipulatePages = () => {
