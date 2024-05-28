2180e864f7c9ca30cd3c91c4dd822c96204fdac9
nazar-r <157609345+nazar-r@users.noreply.github.com>
2024-05-28 20:58:21 +0300

    Update main-navigation.js

--- js/main-navigation.js
+++ js/main-navigation.js
@@ -1,25 +1,20 @@
-const theButton = document.querySelector('.button-navigation');
-const theMainNav = document.querySelector('.main-navigation');
-const theHomePageButton = document.querySelector('.main-navigation__button--home-page__vector');
-const theToDoPageButton = document.querySelector('.main-navigation__button--todo-page__vector');
-const theStatisticsPageButton = document.querySelector('.main-navigation__button--statistics-page__vector');
-
 const renderVectorContainerItems = (element, action, className) => {
     element.classList[action](className);
 };
 
 const manipulateMainNav = () => {
-    theMainNav.classList.contains('hidden') ? renderMainNav() : removeMainNav();
+    const theTasksNav = document.querySelector('.tasks__item--vectors-container-appeared');
+    const theTasksNavUnexist = theTasksNav && theTasksNav.classList.contains('hidden');
+    
+    theMainNav.classList.contains('hidden') && theTasksNavUnexist ? renderMainNav() : removeMainNav();
 };
 
 const renderMainNav = () => {
     if (window.innerWidth <= 650) {
         theButton.style.bottom = '51%';
-    }
-    else if (window.innerWidth >= 650 && window.innerWidth <= 1250) {
+    } else if (window.innerWidth >= 650 && window.innerWidth <= 1250) {
         theButton.style.bottom = '33.7%';
-    }
-    else if (window.innerWidth >= 1250) {
+    } else if (window.innerWidth >= 1250) {
         theButton.style.left = '26%';
     }
 
@@ -33,7 +28,7 @@ const removeMainNav = () => {
         theButton.style.left = '15px';
     } else {
         theButton.style.left = 'calc(50% - 90px)';
-        theButton.style.bottom = '10px';
+        theButton.style.bottom = '20px';
     }
 
     renderVectorContainerItems(theMainNav, 'remove', 'main-navigation__appear');
@@ -44,8 +39,14 @@ const removeMainNav = () => {
     }, 300);
 };
 
+const theButton = document.querySelector('.button-navigation');
+const theMainNav = document.querySelector('.main-navigation');
+const theHomePageButton = document.querySelector('.main-navigation__button--home-page__vector');
+const theToDoPageButton = document.querySelector('.main-navigation__button--todo-page__vector');
+const theStatisticsPageButton = document.querySelector('.main-navigation__button--statistics-page__vector');
+
 theButton.addEventListener('click', manipulateMainNav);
+
 theHomePageButton.addEventListener('click', manipulateMainNav);
 theToDoPageButton.addEventListener('click', manipulateMainNav);
 theStatisticsPageButton.addEventListener('click', manipulateMainNav);
-
