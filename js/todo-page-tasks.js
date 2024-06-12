ef3c5edd2195cf9626f38da97af19bbf8e496bf8
nazar-r <157609345+nazar-r@users.noreply.github.com>
2024-06-12 12:04:34 +0000

    Update JS Block

--- js/todo-page-tasks.js
+++ js/todo-page-tasks.js
@@ -3,7 +3,7 @@ const addTasks = () => {
         renderTask();
         pushTask();
         manipulateTasksHeading();
-        moveToTheLocalStorage();
+        moveToLocalStorage();
         addTasksField.value = '';
     };
 
@@ -19,35 +19,35 @@ const addTasks = () => {
         tasksArray.push(tasksItem);
     };
 
-    const moveToTheLocalStorage = () => {
+    const moveToLocalStorage = () => {
         localStorage.setItem(`Task ${tasksArray.indexOf(tasksItem)}`, addTasksFieldContent);
     };
 
     const tasksItem = document.createElement('div');
     const addTasksFieldContent = addTasksField.value.trim();
     const taskContent = `
-         <div class="tasks__item--edit hidden">Edit</div>
-         <textarea class="tasks__item--field">${addTasksFieldContent}</textarea>
-         <div class="tasks__item--vectors-container hidden"> 
-         <div class="vectors-container__item--set-important hidden">
-         <svg class="item--set-important" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <rect class="item--set-important__button" width="20" height="20" fill="white" />
-         </svg>
-         <p class="item--set-important__article hidden">Set Important</p> 
-         </div>
-         <div class="vectors-container__item--change-text-size hidden">
-         <svg class="item--change-text-size" width="21" height="21" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <circle class="item--change-text-size__button" cx="10" cy="10" r="10" fill="white" />
-         </svg>
-         <p class="item--change-text-size__article hidden">Change Size</p> 
-         </div>
-         <div class="vectors-container__item--delete hidden">
-         <svg class="item--delete" width="20" height="23" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-         <path class="item--delete__button" d="M21 13L-1.14193e-06 25.1244L-8.1987e-08 0.875644L21 13Z" fill="white" />
-         </svg>
-         <p class="item--delete__article hidden">Delete</p> 
-         </div>
-         </div>`
+    <div class="tasks__item--edit hidden">Edit</div>
+    <textarea class="tasks__item--field">${addTasksFieldContent}</textarea>
+    <div class="tasks__item--vectors-container hidden"> 
+    <div class="vectors-container__item--set-important hidden">
+    <svg class="item--set-important" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect class="item--set-important__button" width="20" height="20" fill="white" />
+    </svg>
+    <p class="item--set-important__article hidden">Set Important</p> 
+    </div>
+    <div class="vectors-container__item--change-text-size hidden">
+    <svg class="item--change-text-size" width="21" height="21" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle class="item--change-text-size__button" cx="10" cy="10" r="10" fill="white" />
+    </svg>
+    <p class="item--change-text-size__article hidden">Change Size</p> 
+    </div>
+    <div class="vectors-container__item--delete hidden">
+    <svg class="item--delete" width="20" height="23" viewBox="0 0 21 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path class="item--delete__button" d="M21 13L-1.14193e-06 25.1244L-8.1987e-08 0.875644L21 13Z" fill="white" />
+    </svg>
+    <p class="item--delete__article hidden">Delete</p> 
+    </div>
+    </div>`
 
     manipulateTasksValues();
 };
@@ -57,6 +57,7 @@ const removeTasks = (event) => {
         manipulateDesktopContainer();
         setTimeout(() => manipulateTasksHeading(), 500);
         spliceTasks();
+        removeFromLocalStorage();
     };
 
     const manipulateDesktopContainer = () => {
@@ -97,6 +98,10 @@ const removeTasks = (event) => {
         tasksArray.splice(tasksArray.indexOf(tasksItem), 1);
     };
 
+    const removeFromLocalStorage = () => {
+        localStorage.removeItem(`Task ${tasksArray.indexOf(tasksItem)}`);
+    };
+
     const tasksItem = event.target.closest('.tasks__item');
     const tasksEditItemButton = document.querySelectorAll('.tasks__item--edit');
     const itemDelete = document.querySelectorAll('.vectors-container__item--delete');
@@ -110,24 +115,30 @@ const removeTasks = (event) => {
 const removeTabletTasks = (event) => {
     const manipulateTabletTasksContainer = () => {
         removeTasksEditItemButton();
-        setTimeout(() => removeTasksItem(), 300);
+        setTimeout(() => manipulateTasksItem(), 300);
         removeTasksNavigation();
         removeTheButton();
         setTimeout(() => manipulateTasksHeading(), 500);
+        removeFromLocalStorage();
+    };
+
+    const manipulateTasksItem = () => {
+        theLocalTaskIndex !== -1 ? removeTasksItem() : null;
     };
 
     const removeTasksItem = () => {
-        const theLocalTaskIndex = tasksArray.findIndex(item => item.id === tabletDeleteId);
+        removeItemAnimation();
+        removeItemFromTheTasksArray();
+    };
 
-        if (theLocalTaskIndex !== -1) {
-            const theLocalTask = tasksArray[theLocalTaskIndex];
-            tasksArray.splice(tasksArray[theLocalTaskIndex], 1);
-            theLocalTask.setAttribute('class', 'tasks__item-disappear');
-            setTimeout(() => {
-                theLocalTask.classList.add('hidden'), 500;
-                theTasksContainer.removeChild(theLocalTask);
-            }, 500);
-        };
+    const removeItemAnimation = () => {
+        theLocalTask.setAttribute('class', 'tasks__item-disappear');
+        setTimeout(() => theLocalTask.classList.add('hidden'), 500);
+    };
+
+    const removeItemFromTheTasksArray = () => {
+        tasksArray.splice(tasksArray[theLocalTaskIndex], 1);
+        setTimeout(() => theTasksContainer.removeChild(theLocalTask), 500);
     };
 
     const removeTasksEditItemButton = () => {
@@ -143,11 +154,17 @@ const removeTabletTasks = (event) => {
         theNavButton.style.bottom = '20px';
     };
 
-    const theTasksContainer = document.querySelector('.todo-page__tasks');
+    const removeFromLocalStorage = () => {
+        localStorage.removeItem(`Task ${tasksArray.indexOf(theLocalTask)}`);
+    };
+
     const tabletDelete = event.target;
     const tabletDeleteId = tabletDelete.id;
+    const theLocalTaskIndex = tasksArray.findIndex(item => item.id === tabletDeleteId);
+    const theLocalTask = tasksArray[theLocalTaskIndex];
+    const theTasksContainer = document.querySelector('.todo-page__tasks');
     const tasksEditItemButton = document.querySelectorAll('.tasks__item--edit');
- 
+
     event.target.matches('.item--tablet-delete__button') ? manipulateTabletTasksContainer() : null;
 };
 
